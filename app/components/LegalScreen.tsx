1b960dc82cf3d6e78743e516884db361fbf05dcb
Arif Coskun <arif.coskun@beqom.com>
2025-02-17 17:27:33 -0800

    improving profie screen - improving the screens 2

--- app/components/LegalScreen.tsx
+++ app/components/LegalScreen.tsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { View, ScrollView } from 'react-native';
-import { IconButton, Text, Surface } from 'react-native-paper';
+import { Appbar, Text } from 'react-native-paper';
 import { LegalLinks } from './LegalLinks';
 import { useRouter } from 'expo-router';
-import styles from '../config/styles';
+import globalStyles from '../config/styles';
+import { theme } from '../config/theme';
 
 export default function LegalScreen() {
   const router = useRouter();
 
   return (
-    <View style={styles.layout_container}>
-      <Surface style={styles.header_surface} elevation={2}>
-        <IconButton icon="arrow-left" size={24} onPress={() => router.back()} />
-        <Text style={styles.text_heading2}>Privacy Policy & Terms</Text>
-      </Surface>
-      <ScrollView contentContainerStyle={styles.layout_content}>
-        <Text style={styles.text_subtitle}>
+    <View style={globalStyles.layout_container}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => router.back()} />
+        <Appbar.Content title="Legal & Privacy" />
+      </Appbar.Header>
+
+      <ScrollView 
+        contentContainerStyle={[
+          globalStyles.layout_content, 
+          { paddingVertical: theme.spacing.small }
+        ]}
+      >
+        <Text style={[
+          globalStyles.text_subtitle, 
+          { marginBottom: theme.spacing.small }
+        ]}>
           Review our policies and terms governing your usage.
         </Text>
         <LegalLinks />
