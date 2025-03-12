025d0a4cd6d2b23e69624d2526dd938117ca87d9
Arif Coskun <arif.coskun@beqom.com>
2025-03-11 18:38:11 -0700

    fixing the top navigation on all page v6

--- app/config/CustomAppBar.styles.ts
+++ app/config/CustomAppBar.styles.ts
@@ -1,24 +1,29 @@
 import { StyleSheet, Platform, StatusBar } from 'react-native';
+import { theme } from './theme';
 
 export default StyleSheet.create({
   container: {
-    paddingTop: Platform.OS === 'ios' ? 44 : StatusBar.currentHeight || 0,
+    height: Platform.OS === 'ios' ? 88 : (StatusBar.currentHeight || 0) + 56,
+    width: '100%',
+    position: 'relative',
     zIndex: 10,
   },
   backButtonContainer: {
     position: 'absolute',
-    top: Platform.OS === 'ios' ? 52 : (StatusBar.currentHeight || 0) + 8,
-    left: 8,
-    width: 40,
-    height: 40,
+    top: Platform.OS === 'ios' ? 44 : 0,
+    left: 16, // Material Design standard 16dp horizontal padding
+    width: 48, // Proper touch target (48x48)
+    height: 56, // Standard Material toolbar height
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start', // Align to the start
     zIndex: 10,
   },
   rightContentContainer: {
     position: 'absolute',
-    top: Platform.OS === 'ios' ? 52 : (StatusBar.currentHeight || 0) + 8,
-    right: 8,
+    top: Platform.OS === 'ios' ? 44 : 0,
+    right: 16, // Material Design standard 16dp horizontal padding
+    height: 56, // Standard height
+    justifyContent: 'center',
     zIndex: 10,
   }
 });
