6a71606cd8e15052ac4ffcb17ba7a821b5cffb97
Arif Coskun <arif.coskun@beqom.com>
2025-03-21 06:12:56 -0700

    Adding backend objects v6

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,16 @@
-import * as admin from 'firebase-admin';
 import { setGlobalOptions } from 'firebase-functions/v2';
-import { onUserCreate } from './auth/onUserCreate';
+import * as admin from 'firebase-admin';
+
+setGlobalOptions({
+  region: 'us-central1',
+  maxInstances: 10,
+  minInstances: 0,
+  timeoutSeconds: 60,
+  memory: '256MiB',
+  concurrency: 80
+});
+
+import { onUserCreated } from './auth/onUserCreate';
 import { dailyStats } from './scheduled/dailyStats';
 import { generateMoodInsights } from './api/insights';
 import { getUserStats } from './api/userStats';
@@ -9,18 +19,10 @@ import { sendDailyMeditationReminder } from './scheduled/notificationManager';
 // Initialize Firebase Admin
 admin.initializeApp();
 
-// Set global options for all functions
-setGlobalOptions({
-  region: 'us-central1',
-  maxInstances: 10,
-  minInstances: 0,
-  timeoutSeconds: 60
-});
-
 // Export all functions
 export {
   // Auth functions
-  onUserCreate,
+  onUserCreated,
   
   // Scheduled functions
   dailyStats,
